cfb3cdb1aaab289a746c924132abef22995cd91a
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-31 21:16:35 +0530

    code push

--- Arrays/ExtraQuestion.cpp
+++ Arrays/ExtraQuestion.cpp
@@ -98,7 +98,6 @@
 //         cout << endl;
 //      }
 
-
 // }
 
 // int main(){
@@ -116,47 +115,93 @@
 //      return 0;
 // }
 
-// agar antiClock wise reverse karna hoga to mai aise above wale s karunga okkh and iski t.c O(n^2) hogi okkh!..
+// // agar antiClock wise reverse karna hoga to mai aise above wale s karunga okkh and iski t.c O(n^2) hogi okkh!..
 
-// Maximum subArray nums =  [-2,1,-3,4,-1,2,1,-5,4];
+// // Maximum subArray nums =  [-2,1,-3,4,-1,2,1,-5,4];
 
-#include <iostream>
-#include <climits>
-using namespace std;
+// #include <iostream>
+// #include <climits>
+// using namespace std;
 
-int getMaximum(int arr[], int size) {
-       int ans = INT_MIN;
-       for(int i = 0; i<size; i++){
-           int sum = 0;  // iska sum mai yha p define isliye kar rha hu bcz jab bhi mera i increse hoga tan sum = 0 ho ojayega okkh!.. 
-        for(int j = i; i<size; j++){
-              sum += arr[j];
-              ans = max(ans, sum);
-        }
-        cout << sum << endl;
-       }
-       cout << ans << endl;
-       return ans;
-}
+// int getMaximum(int arr[], int size) {
+//        int ans = INT_MIN;
+//        for(int i = 0; i<size; i++){
+//            int sum = 0;  // iska sum mai yha p define isliye kar rha hu bcz jab bhi mera i increse hoga tan sum = 0 ho ojayega okkh!.. 
+//         for(int j = i; i<size; j++){
+//               sum += arr[j];
+//               ans = max(ans, sum);
+//         }
+//         cout << sum << endl;
+//        }
+//        cout << ans << endl;
+//        return ans;
+// }
 
-int main () {
-    int arr[9] =  {-2,1,-3,4,-1,2,1,-5,4};
+// int main () {
+//     int arr[9] =  {-2,1,-3,4,-1,2,1,-5,4};
 
-    // issme actaully hame karna ye hota hai like ki sum of maximum subArray findout karna hota hai okh!..
-    // to ek loop lagega outer and inner dono okkh!.. and mughe har ek arart k sum ko coompare karke niklana
-    // hota maimm findout karna 
-    // hota hai okkh!>.
+//     // issme actaully hame karna ye hota hai like ki sum of maximum subArray findout karna hota hai okh!..
+//     // to ek loop lagega outer and inner dono okkh!.. and mughe har ek arart k sum ko coompare karke niklana
+//     // hota maimm findout karna 
+//     // hota hai okkh!>.
      
-    int size = 9;
+//     int size = 9;
 
-    int ans02 = getMaximum(arr, size);
+//     int ans02 = getMaximum(arr, size);
 
-    cout << ans02 << endl;
+//     cout << ans02 << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 // isko karne s tle aa rhi hia okkh!..
 
 // iska best solution hoga kaden's algorithm..;
 // and kaden's algo ka t.c O{n} hota hai okkh!..
+//  teen step hota hai kadens k like 
+// first one calculate sum += arr[i];
+// stepp-2 upadte answer okkkh! ans =max(ans , sum);
+// step-2 update sum if(sum < 0) sum = 0;
+
+
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int getMaximumByKadens(int arr[], int size) {
+     int ans = INT_MIN;
+     int sum = 0;
+
+     for(int i = 0; i<size; i++){
+          // yha kadens lagega okkh!.. 
+          // step1: traverse and array and get sum;
+          // step2: upadte ans 
+          // step3: sum < 0 sum = 0; okkh!.
+
+          sum += arr[i];
+          
+          // step2 p toodha sa fasta hai case bcz like isme hame
+          // check karna hota hai ki pehle sum wala likhna hai ya answer wala likhna hai okkh!..
+          // to hamm update hi karte hai pehle answer ko apne bcz -negative wale cases m fass sakte hai kuch cases okkh!..
+
+          ans = max(ans, sum);
+          if(sum < 0){
+             sum = 0;
+          }
+     }
+     return ans;
+}
+
+int main () {
+      int arr[9] =  {-2,1,-3,4,-1,2,1,-5,4};
+     
+    int size = 9;
+
+    int ans02 = getMaximumByKadens(arr, size);
+
+    cout << ans02 << endl;
+
+    return 0;
+}
 
