53ac803c480f84ebbbf2663ac6144af157c58722
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-31 18:19:45 +0530

    code push

--- Arrays/ExtraQuestion.cpp
+++ Arrays/ExtraQuestion.cpp
@@ -60,60 +60,103 @@
 // ek aur ata hai rotate an image of an 2-d array!..
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void getRotatedArrayByAntiClock(int arr[][3], int rowSize, int colSize){
-      // first step jo hota hai wo transpose ka hota hai okkh!.
-      for(int i=0; i<rowSize; i++){
-         for(int j = i; j<colSize; j++){
-               swap(arr[i][j], arr[j][i]);
-         }
-      }
+// void getRotatedArrayByAntiClock(int arr[][3], int rowSize, int colSize){
+//       // first step jo hota hai wo transpose ka hota hai okkh!.
+//       for(int i=0; i<rowSize; i++){
+//          for(int j = i; j<colSize; j++){
+//                swap(arr[i][j], arr[j][i]);
+//          }
+//       }
 
-      // iske swap hone k baad array!>.
-      for(int i = 0; i<rowSize; i++){
-        for(int j = 0; j<colSize; j++){
-              cout << arr[i][j] << " ";
-        }
-        cout << endl;
-     }
+//       // iske swap hone k baad array!>.
+//       for(int i = 0; i<rowSize; i++){
+//         for(int j = 0; j<colSize; j++){
+//               cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//      }
 
-     // ye abovev wala to dekh wo like na transpose ho chukka hai sahii s kkkh abb mugeh siko 90* anticlock wise rotate karna hai okh!..
+//      // ye abovev wala to dekh wo like na transpose ho chukka hai sahii s kkkh abb mugeh siko 90* anticlock wise rotate karna hai okh!..
       
-     for(int j = 0; j<colSize; j++){
-             int top = 0; int bottom = rowSize-1;
-             while(top < bottom){
+//      for(int j = 0; j<colSize; j++){
+//              int top = 0; int bottom = rowSize-1;
+//              while(top < bottom){
                  
-                   swap(arr[top][j], arr[bottom][j]);
-                   top++;
-                   bottom--;
-             }
-        }
+//                    swap(arr[top][j], arr[bottom][j]);
+//                    top++;
+//                    bottom--;
+//              }
+//         }
       
-         for(int i = 0; i<rowSize; i++){
-        for(int j = 0; j<colSize; j++){
-              cout << arr[i][j] << " ";
-        }
-        cout << endl;
-     }
+//          for(int i = 0; i<rowSize; i++){
+//         for(int j = 0; j<colSize; j++){
+//               cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//      }
 
 
-}
+// }
 
-int main(){
-     int arr[3][3] = {
-                  {1,2,3},
-                 {4,5,6},
-                 {7,8,9}
-     };
+// int main(){
+//      int arr[3][3] = {
+//                   {1,2,3},
+//                  {4,5,6},
+//                  {7,8,9}
+//      };
 
-     int rowSize = 3;
-     int colSize = 3;
+//      int rowSize = 3;
+//      int colSize = 3;
      
-     getRotatedArrayByAntiClock(arr, rowSize, colSize);
+//      getRotatedArrayByAntiClock(arr, rowSize, colSize);
 
-     return 0;
-}
+//      return 0;
+// }
 
 // agar antiClock wise reverse karna hoga to mai aise above wale s karunga okkh and iski t.c O(n^2) hogi okkh!..
+
+// Maximum subArray nums =  [-2,1,-3,4,-1,2,1,-5,4];
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int getMaximum(int arr[], int size) {
+       int ans = INT_MIN;
+       for(int i = 0; i<size; i++){
+           int sum = 0;  // iska sum mai yha p define isliye kar rha hu bcz jab bhi mera i increse hoga tan sum = 0 ho ojayega okkh!.. 
+        for(int j = i; i<size; j++){
+              sum += arr[j];
+              ans = max(ans, sum);
+        }
+        cout << sum << endl;
+       }
+       cout << ans << endl;
+       return ans;
+}
+
+int main () {
+    int arr[9] =  {-2,1,-3,4,-1,2,1,-5,4};
+
+    // issme actaully hame karna ye hota hai like ki sum of maximum subArray findout karna hota hai okh!..
+    // to ek loop lagega outer and inner dono okkh!.. and mughe har ek arart k sum ko coompare karke niklana
+    // hota maimm findout karna 
+    // hota hai okkh!>.
+     
+    int size = 9;
+
+    int ans02 = getMaximum(arr, size);
+
+    cout << ans02 << endl;
+
+    return 0;
+}
+
+// isko karne s tle aa rhi hia okkh!..
+
+// iska best solution hoga kaden's algorithm..;
+// and kaden's algo ka t.c O{n} hota hai okkh!..
+
