4f4e9d553048d3c6a2fab9991e511049e616a8bb
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-01 23:57:30 +0530

    coode push

--- VectorStl/Container.cpp
+++ VectorStl/Container.cpp
@@ -1,57 +1,56 @@
 // In Conatiner 6 chize ati hai 
 // Vector, List, queue, stack, set and map,
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// int main() {
-//     // craeteing a vector here!..
-//     vector<int> ans(5,10);
-//     // intailizing a vector size!..
-//     // vector<int> ans(5); // 5 size ka array ban gya hai ye okkh!..
+int main() {
+    // craeteing a vector here!..
+    vector<int> ans(5,10);
+    // intailizing a vector size!..
+    // vector<int> ans(5); // 5 size ka array ban gya hai ye okkh!..
     
-//     // vector<int> ans(5, 0); // 5 size kaarray value uske andar 0 hai 5chi jagah okkh!.. [0,0,0,0,0]; 
+    // vector<int> ans(5, 0); // 5 size kaarray value uske andar 0 hai 5chi jagah okkh!.. [0,0,0,0,0]; 
      
-//    cout <<  *ans.begin() << endl;
-//    cout << *ans.end() << endl; // iska address aa rha hai valuee nahi aa rhi hai okh!..
+   cout <<  *ans.begin() << endl;
+   cout << *ans.end() << endl; // iska address aa rha hai valuee nahi aa rhi hai okh!..
 
-//    // * laagne s aage value pprint hogi us array kii okkh!..
+   // * laagne s aage value pprint hogi us array kii okkh!..
 
 
-//     return 0;
-// }
+    return 0;
+}
 
 // vector mai like agar koi value excess karni padegi to mai * uske aage lga k kar lunga okkh!..
 // for an example ans.begin() isse address ayega us point ka but value chaiye to mai *ans.begin() kar dunga usse array ki value aa jayegi okh!.
 
-// #include <iostream>
-// using namespace std;
-// #include <vector>
+#include <iostream>
+using namespace std;
+#include <vector>
 
-// int main () {
+int main () {
      
-//     vector<int> marks;
+    vector<int> marks;
 
-//     // abb agar mughe marks wale data mai kcuh bhi push karna hoga to mai push_back s kar dunag okkh!..
+    // abb agar mughe marks wale data mai kcuh bhi push karna hoga to mai push_back s kar dunag okkh!..
 
-//     marks.push_back(10);
-//     marks.push_back(20);
-//     marks.push_back(30);
+    marks.push_back(10);
+    marks.push_back(20);
+    marks.push_back(30);
      
-//     // ek isme hi v.begin() ata hai wo first wale k liye hota hai first elemnt k liye okkh!!.
-//     // lats wala agar mugeh chaiye v.end() kar dunga okkh mai!.
+    // ek isme hi v.begin() ata hai wo first wale k liye hota hai first elemnt k liye okkh!!.
+    // lats wala agar mugeh chaiye v.end() kar dunga okkh mai!.
 
-//     vector<int>::iterator it = marks.begin();
+    vector<int>::iterator it = marks.begin();
 
-//      while(it != marks.end()){
-//          cout << *it << " ";
-//          it++;
-//      }
+     while(it != marks.end()){
+         cout << *it << " ";
+         it++;
+     }
 
-//     return 0;
-// }
-
+    return 0;
+}
 
 // List wala bhi ata hai ek!.. list wale ki ek creation bhi dekh l sahi s okkkh!..
 
@@ -127,7 +126,62 @@ int main() {
     // uski postioning deni padegi and then kya insert karna hai wo dena ppadega okkh!..
     // for an example!>.
 
-    nums.insert(nums.begin() + 3, 10);
+     // list mai insertion kuch alg tarikhe s hota hai okkh!..
+
+     // for an example!..
+
+    //  auto it = nums.begin();
+    //  advance(it, 3);
+
+    //  nums.insert(it, 10)
+
+    //  while(it3 != nums.end()){
+    //     cout << *it3 << " ";
+    //     it3++;
+    //  }
+     
 
      return 0;
+}
+
+
+// Queue.. data structure okkh!..
+
+// isme bhi zada kuch nahi hai bass tera empty, size, push, pop, front() back(),  
+// push s data piche lagte jayega okkh! and pop s data delete oga from piche okh!..
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main() {
+    queue<int> q;
+
+    q.push(10);
+    // 10-> 
+    q.push(20);
+    // 10->20
+    q.push(30);
+    // 10->20->30;
+    q.push(40);
+    // 10->20->30->40;
+   
+    q.front() ; // 10 ayega okkh!..
+    q.back(); // 40 ayega okkh!.
+    
+    q.pop(); // always front wala hoga okkh!.. bcz yha p hamne intailze todhi kiya hai like ki front ka hai ya back ka hai okkh! like q.push_back() q.pop_back() etc etc okkh!>.
+    
+    
+    if(q.empty() == true){
+          cout << "kahli hai queue apna okkj!" << " ";
+    }else{
+         cout << "queue khali nahi hai okh!" << " ";
+    }
+    
+    cout << q.size() << endl; // isse quueu ki size aa jaygei okkh!>.
+
+    // iterator ayega abb yha p 
+
+    return 0;
 }
