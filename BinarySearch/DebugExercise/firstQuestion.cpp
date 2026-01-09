124104aa1bea1f98f308a022dd81dd382cc4f3f0
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-10 00:21:00 +0530

    codo added

--- BinarySearch/DebugExercise/firstQuestion.cpp
+++ BinarySearch/DebugExercise/firstQuestion.cpp
@@ -29,3 +29,23 @@ int main() {
 
     return 0;
 }
+
+// sqauree root x; 
+
+int mySqrt(int x) {
+        if (x == 0)
+            return x;
+        int first = -1, last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid*mid  == x)
+                return mid;
+            else if (mid*mid > x) {
+                last = mid-1;
+            }
+            else {
+                first = mid + 1;
+            }
+        }
+        return -1;
+    }
