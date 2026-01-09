58a3fcd826c9eb8d8d7fc1f71a71433409f55bdc
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-09 23:50:05 +0530

    git add

---
+++ BinarySearch/DebugExercise/firstQuestion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+		int l = 0, r = nums.size() - 1;
+		while(l <= r){
+			int mid = l + (r-l)/2;
+			if(nums[mid] == target) return mid;
+			else if(nums[l] <= nums[mid]){
+				if(nums[l] <= target && target<nums[mid]) r = mid-1;
+				else l = mid+1;
+			}
+			else{
+				if(nums[mid]<target && target<=nums[r]) l = mid+1;
+				else r = mid-1;
+			}
+		}
+		return -1; // No need to debug this line.
+	}
+
+int main() {
+      vector<int> nums = {10,20,30,40,50,60};
+      int target = 60;
+
+      int ans = search(nums, target);
+
+      cout << ans << " ";
+
+    return 0;
+}
