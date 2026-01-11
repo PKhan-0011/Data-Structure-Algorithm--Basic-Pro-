0a64da13f18a0adcb6225c7204fb3d122c7a4165
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-11 13:26:58 +0530

    code addedd

---
+++ SortingAlgorithm/Practise/BubbleSort.cpp
@@ -0,0 +1,31 @@
+// Aarray = [44,33,55,22,11];
+
+#include <iostream>
+using namespace std;
+
+void getSortedArray(int arr[], int size){
+    for(int i = 0; i<size-1; i++){
+         for(int j = 0; j< size-i-1; j++){
+              if(arr[j] > arr[j+1]){
+                 swap(arr[j], arr[j+1]);
+              }
+         }
+    }  
+}
+
+void printArray(int arr[], int size){
+         for(int i = 0; i<size; i++){
+             cout << arr[i] << " "; 
+         }
+     };
+
+int main() {
+    int arr[] = {44,33,55,22,11};
+    int size = 5;
+     
+     getSortedArray(arr, size);
+
+     printArray(arr, size);
+
+    return 0;
+}
