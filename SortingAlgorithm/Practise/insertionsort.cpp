f0cc080c5a82bfd62f5c7c820934ed9f40665745
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-11 14:28:24 +0530

    code push

---
+++ SortingAlgorithm/Practise/insertionsort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void getSortedArrayByInsertionSort(int arr[], int size){
+    for(int i = 0; i<size-1; i++){
+            int ansIndex = i;
+         for(int j = i+1; j< size; j++){
+              if(arr[j] < arr[ansIndex]){
+                  ansIndex = j;
+              }
+         }
+         swap(arr[ansIndex], arr[i]);
+    }  
+}
+
+void printArray(int arr[], int size){
+         for(int i = 0; i<size; i++){
+             cout << arr[i] << " "; 
+         }
+     };
+
+int main() {
+    int arr[] = {44,33,55,22,11};
+    int size = 5;
+     
+     getSortedArrayByInsertionSort(arr, size);
+
+     printArray(arr, size);
+
+    return 0;
+}
