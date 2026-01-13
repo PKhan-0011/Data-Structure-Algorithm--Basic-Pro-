e3f3ca0b5a854196b6a9473a8b94c22fa69702d7
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-13 12:20:37 +0530

    code added

--- BinarySearch/MegaClass.cpp
+++ BinarySearch/MegaClass.cpp
@@ -1,41 +1,57 @@
 // Sqrt with precison;
 
-// first question to smjh le aise hi hai without precison isko sahi s dekh;
+// 63 ka answer 7.93256342 aisa kuh ana chaiye kar k dekh!..
+
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+double getPrecisionValue(int value){
+      double s = 0;
+      double e = value;
+      double ans = 0.001;
+      while((e-s) >= 0.00001){
+          double mid = (s + e)/2;
+
+          if(mid*mid <= value){
+             ans = mid;
+              s = mid;
+          }
+          else {
+             e = mid;
+          }
+      }
+      return ans;
+}
+
+int main () {
+     int value = 63;
+     double ans2 = getPrecisionValue(value);
+     printf("precision upto 8th term: %.8f\n", ans2);
+
+     cout << ans2 << " ";
+
+     return 0;
+}
+
+// 2nd quetion ki bat agar karenge to usme!..
+// divident divisor wala question ayega okkh!..
 
 #include <iostream>
 using namespace std;
 
-int getMinimumValue(int target, int ansIndex){
-    int s = 0;
-    int e = 63;
-    int mid = s + (e-s)/2;
-
-    while( s <= e){
-        int prod = mid*mid;
-
-        if(prod == target){
-            return mid;
-        }
-        else if(prod > target){
-            e = mid-1;
-        }
-        else{
-            // iska matlb ye hai ki prod chota hai target s!;;
-            s = mid + 1;
-            ansIndex = mid;
-        }
-        mid = s + (e-s)/2;
-    }
-    return ansIndex;
+int getQuoitent(int divident, int divisor) {
+         
 }
 
-int main(){
-     int target = 63;
-      int ansIndex = -1;
-      
-      int ans = getMinimumValue(target, ansIndex);
-
-      cout << ans << " ";
+int main() {
+    int divident = 46;
+    int divisor = 2;
+    
+    // yha p hamm quotient ko hi mid bol rhe hai okh!.
+    int ans = getQuoitent(divident, divisor);
+    cout << ans << " ";
 
     return 0;
 }
