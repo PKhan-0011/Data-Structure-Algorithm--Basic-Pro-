fdde8c79adf3c5286e31d3d8f3885ae934845990
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-15 23:12:15 +0530

    code added

--- BinarySearch/DebugExercise/3rdquestion.cpp
+++ BinarySearch/DebugExercise/3rdquestion.cpp
@@ -1,20 +1,129 @@
 //Debug the code. [F](https://  leetcode.com/problems/find-minimum-in-rotated-sorted-array/)ind minimum in rotated sorted array. (Sorting was in ascending order).
-// #include <iostream>
-// using namespace std;
-// #include <vector>
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size()-1, ans = start;
+        while(nums[start] > nums[end]) {
+            int mid = (start + end) >> 1; // riight shift chaiye yha p okk!..
+            if(nums[mid] <= nums[start] && nums[mid] <= nums[end]) 
+                end = mid;
+            else if(nums[mid] >= nums[start] && nums[mid] >= nums[end])
+                start = mid + 1;          
+            else return nums[mid];
+            ans = start;
+        }
+        return nums[ans];
+    }
 
 
-// int findMin(vector<int>& nums) {
-//         int start = 0, end = nums.size()-1, ans = start;
-//         while(nums[start] > nums[end]) {
-//             int mid = (start + end) >> 1; // riight shift chaiye yha p okk!..
-//             if(nums[mid] <= nums[start] && nums[mid] <= nums[end]) 
-//                 end = mid;
-//             else if(nums[mid] >= nums[start] && nums[mid] >= nums[end])
-//                 start = mid + 1;          
-//             else return nums[mid];
-//             ans = start;
-//         }
-//         return nums[ans];
-//     }
+
+#include <iostream>
+using namespace std;
+
+int getPivotIndex(int arr[], int size){
+    int s = 0;
+    int e = size-1;
+
+    while(s <= e){
+          int mid = s + (e-s)/2;
+          // yha p generally teen cases bante hai pivot naikalne k 
+          // like kya pta wo usi element p khda ho like mid p hi okkh!..
+          // mid > mid +1  return mid and if mid -1 > mid return mid-1 karna padega okkh!..
+          if( mid + 1 < size && arr[mid] > arr[mid+1]){
+                return mid;
+          }
+
+          else if (mid-1 >= 0 && arr[mid] < arr[mid-1]){
+               return mid -1;
+          }
+
+          if(arr[s] > arr[mid]){
+              // mai second line mai hu mughe piche jana hai okkj!.
+              e = mid -1;
+          }
+          else {
+               s = mid +1;
+          }
+    }
+    return -1;
+}
+
+int main() {
+      int arr[] = {4,5,6,7,0,1,2};
+      int size = 7;
+      int target = 0;
+      int ans = getPivotIndex(arr, size);
+       cout << ans << " ";
+        
+       // yha s jo pivot index hai wo 3 aa rha hai okkh!..
+       
+       // yha p 2 condtion banegi okkh!. first one is 
+       // target jo hai wo first line mai hai ya ya fiir target jo hai wo 2nd line mai hai okkh!.
+
+       if(target >= arr[0] && target <= arr[ans]){
+            int s = 0; 
+            int e = ans;
+            while(s <= e){
+
+
+                 int mid = s + (e-s)/2;
+                  
+                 if(s == e){
+                      if(arr[mid] == target){
+                           cout << mid << " ";
+                           return mid;
+                      }
+                      else {
+                          return -1;
+                      }
+                 }
+
+                  if(arr[mid] == target){
+                      cout << mid  << " ";
+                       return mid;
+                  }
+                  else if(arr[mid] > target){
+                      e = mid-1;
+                  }
+                  else {
+                     s = mid + 1;
+                  }
+            }
+       }
+
+       if(target >= arr[ans+1] && target <= arr[size-1]){
+             int s = ans+1;
+             int e = size-1;
+             while(s <= e){
+                  int mid = s + (e-s)/2;
+
+                   if(s == e){
+                      if(arr[mid] == target){
+                           cout << mid << " ";
+                           return mid;
+                      }
+                      else {
+                          return -1;
+                      }
+                 }
+
+                  if(arr[mid] == target){
+                     cout << mid << " ";
+                     return mid;
+                  }
+                  else if(arr[mid] > target){
+                       e = mid + 1;
+                  }
+                  else {
+                     s = mid + 1;
+                  }
+             }
+       }
+
+      return 0;
+}
+
+
 
--- BinarySearch/PractiseQuestion/lect03.cpp
+++ BinarySearch/PractiseQuestion/lect03.cpp
@@ -3,47 +3,52 @@
 // int arr[] = {10, 3, 40, 20, 50, 70} key = 40; 
 // output = 2;
 
-// #include <iostream>
-// using namespace std;
+// bass itna dhyan rakhio like ki ye sorted Array hota hai okkh!. 
+// which means right side mai badi value hogi sari okkh!..
+// and left part mai choti value hongi sari okkh!.
 
-// int getTarget(int arr[], int target, int n){
-//     int s = 0;
-//     int e = n-1;
 
-//     while(s <= e){
-//           int mid = s + (e-s)/2;
+#include <iostream>
+using namespace std;
 
-//           if(arr[mid] == target){
-//               return mid;
-//           }
-//           else if (arr[mid + 1] == target){
-//               return mid + 1;
-//           }
-//           else if (arr[mid - 1] == target){
-//              return mid -1;
-//           }
+int getTarget(int arr[], int target, int n){
+    int s = 0;
+    int e = n-1;
 
-//           if(target > arr[mid]){
-//               s = mid + 1;
-//           }
-//           else {
-//              e = mid -1;
-//           }
-//     }
-//     return -1;
-// }
+    while(s <= e){
+          int mid = s + (e-s)/2;
 
-// int main() {
+          if(arr[mid] == target){
+              return mid;
+          }
+          else if (arr[mid + 1] == target){
+              return mid + 1;
+          }
+          else if (arr[mid - 1] == target){
+             return mid -1;
+          }
+
+          if(target > arr[mid]){
+              s = mid + 1;
+          }
+          else {
+             e = mid -1;
+          }
+    }
+    return -1;
+}
+
+int main() {
     
-//     int arr[] = {10,3,40,20,50,70,80};
-//     int n = 7;
-//     int target = 80;
+    int arr[] = {10,3,40,20,50,70,80};
+    int n = 7;
+    int target = 80;
 
-//     int ans = getTarget(arr, target, n);
+    int ans = getTarget(arr, target, n);
 
-//     cout << ans << " ";
-//     return 0;
-// }
+    cout << ans << " ";
+    return 0;
+}
 
 
 // 2nd question!.. find Odd occurence! of an given array!..
@@ -55,6 +60,11 @@
 // ye O(n) mai ho sakta hai okkh.. by xor,
 // ye O(n) mai also ho sakta hai.. by map..
 // but hame O(logn) mai find out karna hai isko ye dhyan rakhna hai okkh!..
+
+// odd occurence wala ata hai ek mere pass okkh!..
+// yha p element merre odd ellemnt return karne hai okkh!..
+// Pair ayega yha okkh!>. wo pair k bich agar ek hi element ayega to uske aage odd pair first value ane laeggi
+// but ek hi element s pehle sare value first even p ayenge!.
 
 #include <iostream>
 #include <unordered_map>
@@ -107,10 +117,36 @@ int getOddOccurence(int arr[], int size){
 int main(){  
     int arr[] = {1,2,2,3,3,4,4,5,5,3,3,4,4};
     int size = 13;
-     
+      
     int ans = getOddOccurence(arr, size);
 
     cout << ans << " ";
 
     return 0;
 }
+
+// mere pass ek auur question.. ata hai like divident and divisor wala okkh!..
+// divident = 44, divisor= 3;
+int getAnswer(int Divident, int Divisor){
+      int s = -Divident;
+      int e = Divident;
+      int ans = 0;
+      while(s <= e){
+           int mid = s + (e-s)/2;
+           if(mid* Divisor == Divident){
+               return mid;
+           }
+
+           else if(mid*Divisor > Divident){
+                e = mid-1;
+           }
+           else{
+               ans = mid;
+               s = mid+1;
+           }
+      }
+      return ans;
+}
+
+
+
