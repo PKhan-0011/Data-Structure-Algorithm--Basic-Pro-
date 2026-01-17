f2a560281d5a388b3471876ae815114e529bc3dc
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-17 21:26:58 +0530

    code push

---
+++ Pointers/lect01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+    // int a = 5;
+    // int b = a;
+    // int* c = &a; // ye pointer ho gya hai ab ye address store karega okh!..
+    //  cout <<  sizeof(c) << " ";
+    // cout << a << " ";
+    // cout << &a << " "; // ye wala hame address dega ye dhyan rakhio okkh!..
+      
+    // bad Practise!..
+    // int a;
+    //  int * ptr;
+    //  cout << * ptr << " "; 
+    // return 0;
+}
