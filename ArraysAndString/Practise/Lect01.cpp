f97405eae24cc923f280b6342866c227e23bdcf5
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-22 18:15:07 +0530

    code added

--- ArraysAndString/Practise/Lect01.cpp
+++ ArraysAndString/Practise/Lect01.cpp
@@ -45,7 +45,7 @@ void getNewArray(char arr[]){
 }
 
 int main() {
-   char arr[1000];
+   char arr[1000] = {''};
 
    getNewArray(arr);
 
@@ -55,3 +55,100 @@ int main() {
 
    return 0;
 }
+
+// Convert To Lower case!.. Leetocode 709;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int size = s.length();
+        for(int i = 0; i<size; i++){
+            int ch = s[i];
+            if(ch >= 'A' && ch <= 'Z'){
+                 ch = ch - 'A' + 'a';
+                 s[i] = ch;
+            }
+        }
+        return s;
+    }
+};
+
+// Convert toUpper case!..
+class Solution {
+     public: 
+     string toUpperCase(string s){
+        int size = s.size();
+        for(int i = 0; i<size; i++){
+             int ch = s[i];
+             if(ch >= 'a' && ch <= 'z'){
+                  ch = ch - 'a' + 'A';
+                  s[i] = ch;
+             }
+        }
+        return s;
+     }
+};
+
+// reverse of an array!.. [love], [evol];
+// mugeh yha p ek doubt aa rha tha ki mughe yha llength kyu niklana hai!..
+// bcz length nikalne s hamm!, easily looping karke valuue nikl lenge otherwise mughe mannuly dena padega okkh!..
+
+class Solution {
+     public: 
+     string toReverseArray(string s){
+        int size = s.size();
+        // dekh mughe na yha p size niklna padega agar size nhai hota to 
+        // bcz is size s hi mai reverse kar paunga and hamm upar size = 1000; de de rahe the wo errror de dega dhyan s kario okkh!..
+        
+        int i = 0;
+        int j = s.size();
+
+        while(i <= j){
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+         return s;
+     }
+};
+
+// leetcode question..!
+#include <vector>
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i <= j){
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+    }
+};
+
+// Palindrome wala question!..
+
+class Solution {
+     public: 
+     bool toReverseArray(string s){
+        int size = s.size();
+        
+        
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i <= j){
+            if(s[i] == s[j]){
+                  i++;
+                  j--;
+            }
+            else {
+                return false;
+            }
+        }
+         return true;
+     }
+};
