31f55a7b10eee351a0ee7e181665b8521915ae7d
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-25 02:43:32 +0530

    code push

--- ArraysAndString/Practise/Lect02.cpp
+++ ArraysAndString/Practise/Lect02.cpp
@@ -37,3 +37,40 @@ public:
 };
 
 // T.c O(n/k (n-k)); ye banega okkh!..
+
+
+// Valid palindrome2!..
+
+class Solution {
+public:
+   
+   bool checkPalindrome(string s, int i, int j){
+         while(i <= j){
+            if(s[i] == s[j]){
+                 i++;
+                 j--;
+            }
+          else   return false;
+         }
+         return true;
+   }
+     
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length()-1;
+        
+        while(i <= j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else{
+                int ans1 = checkPalindrome(s, i+1, j);
+                int ans2 = checkPalindrome(s, i, j-1);
+
+                return ans1 || ans2;
+            }
+        }
+         return true;
+    }
+};
