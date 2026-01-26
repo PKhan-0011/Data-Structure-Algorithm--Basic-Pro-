8ce25bfbedfe2027f4799961a4a79c743be72cf6
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-01-26 21:56:27 +0530

    code added

---
+++ ArraysAndString/Practise/Lect03.cpp
@@ -0,0 +1,50 @@
+// Decode Message!..
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> mapping;
+         
+         char space = ' ';
+         mapping[space] = space;
+
+        char start = 'a';
+        int index = 0;
+
+        // 1st step jo ohai wo like mapping create karo!. with 
+        // there name and alphabet!..
+         
+         while(start <= 'z' && index < key.length()){
+               // first value nikalna padega key ka okh!..
+               char firstValue = key[index];
+
+               // ek check karna padega ki yha p already conection with alhabe to nahi bna hia na?
+               if(mapping.find(firstValue) != mapping.end()){
+                   index++;
+               }
+               else{
+                    mapping[firstValue] = start;
+                    start++;
+                    index++;
+               }
+
+         }
+
+        // abb iski mapping to dkeh ho gayi hai okkh!...
+         // to is case m mughe decoded value niklni hai!.
+
+         string ans = "";
+         for(int i = 0; i<message.length(); i++){
+              char messageValues = message[i];
+              char mappedValue = mapping[messageValues];
+              ans.push_back(mappedValue);
+         }
+        return ans;
+    }
+};
