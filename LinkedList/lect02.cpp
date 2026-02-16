49a12cbb1ee976abca182264f67b22e1b0a3331d
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-02-16 17:29:47 +0530

    code added

--- LinkedList/lect02.cpp
+++ LinkedList/lect02.cpp
@@ -37,7 +37,18 @@ void InsertionAtHead(int value, Node* &head, Node* &tail){
 
 void insertAtTail(int value, Node* &head, Node* &tail){
    // emplty wala case okkh!..
-   
+   if(head == NULL && tail == NULL){
+       Node* newNode = new Node(value);
+       tail = newNode;
+       head = newNode;
+   }
+   // but agar empty nahi hai is case m kya karenge ye batao okkh!..
+   else{
+        Node* newNode = new Node(value);
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+   }
 }
 
 void print(Node* head){
@@ -56,15 +67,112 @@ void printReverse(Node* tail){
       }
 }
 
+int getLength(Node* head, Node* tail) {
+     Node* temp = head;
+     int count = 0;
+     while(temp->next != NULL){
+          count++;
+          temp = temp->next;
+     }
+     return count;
+}
+
+void InsertAtPosition(int value, Node* &head, Node* &tail, int position){
+     // agar postion one hui to ye head p laega okkh!.
+     int len = getLength(head, tail);
+     if(position == 1){
+          InsertionAtHead(value, head, tail);
+     }
+     
+     else if (position == len + 1){
+           // mai last p hu okkh! abb mughe yha p tail wla lga dunga okkh!..
+           insertAtTail(value, head, tail);
+     }
+
+     else{
+           Node* newNode = new Node(value);
+           Node* temp = head;
+           for(int i = 0; i<position-2; i++){
+               temp = temp->next;
+           }
+           Node* forheadNode = temp->next;
+           temp->next = newNode;
+           newNode->prev = temp;
+           newNode->next = forheadNode;
+           forheadNode->prev = newNode;
+     }
+}
+
+void SearchTarget(Node* head, Node* tail, int target){
+        Node* temp = head;
+        while(temp->next != NULL){
+             if(temp->data == target){
+                  cout << temp->data << " ";
+             }
+              temp = temp->next;
+        }
+}
+
+// deletion of linkedlist!..
+void deletetionOfNodes(Node* head, Node* tail, int position){
+        // yha p multiple cases banenge okkh!.
+        if(head == NULL && tail == NULL){
+            // ye emppty wali bat chit hai okkh!..
+            cout << "node hi empty hai mere bhai!.." << " ";
+        }
+        else if(head == tail){
+               // iska mtlb ye hai like ki head aur tail dono hi same point p hai okkh!..
+                Node* temp = head;
+                head = NULL;
+                tail = NULL;
+                delete temp;
+        }
+
+        else {
+              // agar man le kahi bich m hua wo tab kaise larunga ye batao okkh!..
+               if(position == 1){
+                    // ye head wala hai okkh !..
+                    Node* temp = head;
+                    head = temp->next;
+                    temp->next = NULL;
+                    head->prev = NULL;
+                    delete temp;
+               }
+               else{
+                   // agar postion kahi bich mai tab kya karna hai okkh!..
+                   Node* temp = head;
+                   for(int i = 0; i<position-2; i++){
+                       temp = temp->next;
+                   }
+                   Node* deleteNode = temp->next;
+                   Node* NextNode = deleteNode->next;
+                   temp->next = NextNode;
+                    NextNode->prev = temp;
+                    deleteNode->next = NULL;
+                     deleteNode->prev = NULL;
+                     delete deleteNode;
+               }
+        }
+}
+
 int main(){
     Node* head = NULL;
     Node* tail = NULL;
 
-    InsertionAtHead(10, head, tail);
-    InsertionAtHead(20, head, tail);
-    InsertionAtHead(30, head, tail);
+    // InsertionAtHead(10, head, tail);
+    // InsertionAtHead(20, head, tail);
+    // InsertionAtHead(30, head, tail);
+    
+    int ans = getLength(head, tail);
 
-    print(head);
+    int position = 3;
+    insertAtTail(10,head, tail);
+    insertAtTail(20, head, tail);
+    InsertAtPosition(40, head, tail, position);
+
+    SearchTarget(head, tail, 50);
+
+    print(tail);
      cout << endl;
     printReverse(tail);
 
