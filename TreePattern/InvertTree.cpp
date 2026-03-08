ebeee716d7f108d6334de2e7ff608b621e56cccd
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-03-08 21:37:25 +0530

    code added

--- TreePattern/InvertTree.cpp
+++ TreePattern/InvertTree.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-class solution {
+class Solution {
     public: 
     int val;
     TreeNode* left;
@@ -13,7 +13,45 @@ class solution {
 
 class Solution {
 public:
-    
+     
+   vector<int>  levelOrderOnAboveTree(TreeNode* nextRoots){
+           if(nextRoots == nullptr){
+              return {};
+           }
+
+           queue<TreeNode*>q;
+           vector<int> ans;
+           q.push(nextRoots);
+
+           while(!q.empty()){
+                int size = q.size();
+                vector<int> temp(size);
+
+                while(size--){
+                    TreeNode* val = q.front();
+                                      q.pop();
+
+                                      temp.push_back(val);
+                       // abb mere temp mai kuch aisa hai!.. [2]
+                       
+                       // yha p check hoga like ki ye iska Treenode ka left and right null to nahi
+                       // agar null nahi hai to wo loop m jayega okkh!..
+                       if(val->left != nullptr){
+                            q.push(val->left);
+                       }
+
+                       if(val->right != nullptr){
+                         q.push(val->right);
+                       }
+                }
+               for(auto x: temp){
+                 ans.push_back(x);
+               }
+                 
+           }
+           return ans;
+   }
+
     TreeNode* invertTree02(TreeNode* root1, TreeNode* root2){
            if(root1 == nullptr && root2 == nullptr){
                   return;
@@ -37,7 +75,7 @@ public:
         TreeNode* nextRoots = invertTree02(TreeNode* root->left, TreeNode* root->right);
 
         // abb yha s jo node extra banega usme level order traversal lag jayega okkh!..
-       
+          vector<int> array = levelOrderOnAboveTree(TreeNode* nextRoots);     
 
     }
 };
