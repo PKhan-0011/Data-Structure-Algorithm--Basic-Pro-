c01f22540159f039672ab14c3bb12f580ff5e4e6
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2026-04-19 23:59:57 +0530

    code added

--- Heap/PairElement.cpp
+++ Heap/PairElement.cpp
@@ -1,10 +1,16 @@
+
 // pair wala chiaye isme!..
 
-bool cmp()(vector<pair<int, string>> &a, vector<pair<int, string>> &b){
+#include <iostream>
+using namespace std;
+
+
+struct cmp
+{
+    bool operator()(vector<pair<int, string>> &a, vector<pair<int, string>> &b){
        if(a.first != b.first){
             return a.first > b.first; // min heap ban rhi hai isse!.. jo chota hoga wo upar ayega!.
        }
        return a.second > b.second;
 } 
-
-priority_queue<pair<int, string>, vector<pair<int, string>>, cmp> pq;
+};
